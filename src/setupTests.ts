96ae99ee8d495db72fd78b4b66f2b46b33a1ef5a
Tanish Baidya <tbaidya01@qub.ac.uk>
2025-05-22 11:43:24 +0100

    💚 Make  setUpTests an empty module

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 /// <reference types="@vitest/browser/providers/playwright" />
+export {};
